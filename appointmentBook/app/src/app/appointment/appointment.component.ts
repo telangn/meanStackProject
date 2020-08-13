f668591270781716df75011bec1be07c1b676e7d
Ninad Telang <telangn@gmail.com>
2020-08-13 13:01:39 -0700

    Initial Commit

---
+++ appointmentBook/app/src/app/appointment/appointment.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AppointmentsService } from '../appointments.service';
+import { Appointment } from '../Appointment';
+@Component({
+  selector: 'app-appointment',
+  templateUrl: './appointment.component.html',
+  styleUrls: ['./appointment.component.css']
+})
+export class AppointmentComponent implements OnInit {
+
+  public successMsg: string;
+  public errorMsg: string;
+  public appointmentDate: string;
+  public name: string;
+  public email: string;
+
+  constructor(private appointmentService: AppointmentsService) { }
+
+  ngOnInit(): void {
+  }
+
+  createAppointment() {
+    this.successMsg = '';
+    this.errorMsg = '';
+    this.appointmentService.createAppointment(this.appointmentDate, this.name, this.email)
+    .subscribe((createdAppointment: Appointment) => {
+      this.appointmentDate = '';
+      this.name = '';
+      this.email = '';
+      const appointmentDate = new Date(createdAppointment.appointmentDate).toDateString();
+      this.successMsg = `Appointment Booked successfully for ${appointmentDate}`;
+    },
+    (error: ErrorEvent) => {
+      this.errorMsg = error.error.message;
+    });
+  }
+
+}
